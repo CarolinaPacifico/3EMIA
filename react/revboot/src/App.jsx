fcb18cff1a5188e188257ca1ddf5e15b9787d86e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-22 13:43:15 -0300

    Revisão de BootStrap em React

---
+++ react/revboot/src/App.jsx
@@ -0,0 +1,16 @@
+import NavProjeto from './components/NavProjeto'
+import CardComponent from './componets/Cards'
+import BotaoComponent from './componets/Botao'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <NavProjeto/>
+      </div>
+    </>
+  )
+}
+
+export default App
